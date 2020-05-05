632ce4d009122917d85dd611c5b34209c6675fb1
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-05 14:44:26 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,13 +9,26 @@ class Backer
   def initialize(name)
     @name = name
     @backed_projects = []
+    save
+  end
+  
+  def save
+    @@all << self
   end
   
   def back_project(some_project)
-    #binding.pry
-    @backed_projects << some_project
-    some_project.add_backer(self)
-    @backed_projects
+    if Project.all.any?{|project| project.name == some_project.name}
+      self.backed_projects << some_project
+      some_project.backers << self
+      some_project
+    else
+      some_project = Project.new(some_project)
+      some_project.backers << self
+      self.backed_projects << some_project
+      some_project
+    end
   end
+  
+  
   
 end
