feed3994c8030e045ea9c3427af57ee16743820a
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-05 14:28:45 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Backer
+  
+  attr_accessor
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(some_project)
+    binding.pry
+    @backed_projects << some_project
+    some_project.add_backer(self)
+  end
+  
+end
---
+++ lib/project.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Project
+  
+  attr_accessor
+  attr_reader :title
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(some_backer)
+    binding.pry
+    @backers << some_backer
+    some_backer.back_project(self)
+  end
+  
+  
+  
+end
