6a20486474ba6f781be4cb5fd0b1242125819d86
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-05 14:54:48 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -17,16 +17,7 @@ class Backer
   end
   
   def back_project(some_project)
-    if Project.all.any?{|project| project.title == some_project.title}
-      self.backed_projects << some_project
-      some_project.backers << self
-      some_project
-    else
-      some_project = Project.new(some_project)
-      self.backed_projects << some_project
-      some_project.backers << self
-      some_project
-    end
+    
   end
   
   
